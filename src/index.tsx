720c8bfd06a2f2ae1b997dc6ad49dd468691d1c8
dmltls94@gmail.com <dmltls94@gmail.com>
2021-07-13 14:25:29 +0900

    부트스트랩 연동

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css'
+import { Button } from 'react-bootstrap';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Button>부트스트랩?</Button>
   </React.StrictMode>,
   document.getElementById("root")
 );
