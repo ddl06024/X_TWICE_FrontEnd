c67602b74d30bd0618c249a3de4893061a32c969
dmltls94@gmail.com <dmltls94@gmail.com>
2021-07-20 21:23:24 +0900

    헤더 추가(세세한건 추후 수정예정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 import HelloWorld from "./components/HelloWorld";
 import RenderForm from "./components/RenderForm";
 
 const App: React.FC<{}> = () => {
   return (
     <>
-      <HelloWorld />
+      <Header />
       <RenderForm />
     </>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, Col, Container, Form, FormControl, Nav, Navbar, NavDropdown } from "react-bootstrap";
+//d
+const Header: React.FC<{}> = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Container>
+                <Col><Navbar.Brand href="#home">로고이름</Navbar.Brand></Col>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Col ><Nav className="me-auto">
+                    <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="mr-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                    </Nav>
+                    </Col>
+                    
+                    <Nav>
+                    <Nav.Link href="#Login">Login</Nav.Link>
+                    <Nav.Link eventKey={2} href="#RegisterAccount">
+                        RegisterAccount
+                    </Nav.Link>
+                    </Nav>
+                 
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
