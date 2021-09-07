981c1d361f77221d5227fc56d7fea02b3a38652c
dmltls94@gmail.com <dmltls94@gmail.com>
2021-09-07 16:24:46 +0900

    리눅스 서버에 올리기

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const root = path.join(__dirname, "build");
+
+app.use("/", express.static(root));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(__dirname + "/build/", "index.html"));
+});
+
+app.listen(4004);
